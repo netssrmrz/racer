c53e2cb8e44d1fef3ec9a2ad1a0e67591ec28d6e
Esteban Ramirez <netssrmrz@yahoo.com.au>
2015-12-17 02:31:51 +1000

    updates

--- src/rs/projecta/activity/Play_Activity.java
+++ src/rs/projecta/activity/Play_Activity.java
@@ -12,6 +12,7 @@ implements
   public rs.projecta.level.Level curr_level;
   public rs.projecta.object.Player player;
   public float max_d, prev_d;
+  public android.widget.LinearLayout main_view;
 
   @Override
   public void onCreate(android.os.Bundle saved_state)
@@ -19,16 +20,16 @@ implements
     super.onCreate(saved_state);
 
     //android.util.Log.d("Test_Activity.onCreate()", "Entered");
-    this.max_d=0;
-    this.prev_d=0;
-    
+    this.requestWindowFeature(android.view.Window.FEATURE_NO_TITLE);
     this.getWindow().addFlags(
       android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    this.requestWindowFeature(android.view.Window.FEATURE_NO_TITLE);
     this.getWindow().setFlags(
       android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN,
       android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN);
-      
+    this.getWindow().getDecorView().setBackgroundColor(0xff000000);
+    
+    this.max_d=0;
+    this.prev_d=0;
     this.curr_level=rs.projecta.level.Level.Get(this);
     this.world=new rs.projecta.world.World(this, this.curr_level);
     
@@ -37,9 +38,26 @@ implements
     
     this.gfx_view = new rs.projecta.view.Game_View(this, this.world);
     
-    //com.google.android.gms.ads.AdView
-        
-    this.setContentView(gfx_view, 
+    com.google.android.gms.ads.AdView mAdView =
+      new com.google.android.gms.ads.AdView(this);
+    mAdView.setAdSize(com.google.android.gms.ads.AdSize.SMART_BANNER);
+    mAdView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+    com.google.android.gms.ads.AdRequest adRequest = 
+      new com.google.android.gms.ads.AdRequest.Builder().build();
+    mAdView.loadAd(adRequest);
+    
+    this.main_view=new android.widget.LinearLayout(this);
+    this.main_view.setOrientation(android.widget.LinearLayout.VERTICAL);
+    this.main_view.addView(mAdView,
+      new android.widget.LinearLayout.LayoutParams(
+        android.widget.LinearLayout.LayoutParams.MATCH_PARENT,
+        0, 10f));
+    this.main_view.addView(this.gfx_view,
+      new android.widget.LinearLayout.LayoutParams(
+        android.widget.LinearLayout.LayoutParams.MATCH_PARENT,
+        0, 90f));
+    
+    this.setContentView(main_view, 
       new android.widget.LinearLayout.LayoutParams(
         android.widget.LinearLayout.LayoutParams.MATCH_PARENT,
 				android.widget.LinearLayout.LayoutParams.MATCH_PARENT, 1f));
